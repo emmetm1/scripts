1487f389eeee0a5cd861553b18e293fbd5f63914
emmetm1 <43273179+emmetm1@users.noreply.github.com>
2022-10-31 14:40:15 -0500

    Add comments and error checking

--- process-check.sh
+++ process-check.sh
@@ -4,29 +4,45 @@ declare -a host_list
 declare -a failed_hosts
 declare -i fail_count=0
 
+INPUT_FILE="./host_list.txt"
+
+# read in newline seperated file 
 while IFS= read -r line; do
-    host_list+=("$line")
-done < ./host_list.txt
+  host_list+=("$line")
+done < $INPUT_FILE
 
-for host in ${host_list[@]}; do
-  ssh root@$host -t "pgrep $1" > /dev/null
-  if [ $? -gt 0 ] 
+if [ ${#host_list[@]} -gt 0 ]
+then
+  # ssh into each remote host and check if the user specified process is running
+  for host in ${host_list[@]}; do
+    ssh root@$host -t "pgrep $1" > /dev/null
+    if [ $? -gt 0 ] 
+    then
+      fail_count=fail_count+1
+      failed_hosts+=($host)
+    fi
+  done
+
+  # if given process is not running on all hosts create a list of failed hosts
+  if [ $fail_count -gt 0 ]
   then
-    fail_count=fail_count+1
-    failed_hosts+=($host)
+    echo "Number of hosts checked: ${#host_list[@]}"
+    echo "Total number of failed hosts: $fail_count."
+    echo "Please see failed_hosts.txt for a complete list of failures"
+
+    echo "All other hosts passed check"
+
+    echo "" >> failed_hosts.txt
+    echo "list generated on: " >> failed_hosts.txt
+    date >> failed_hosts.txt
+    printf '%s\n' "${failed_hosts[@]}" >> failed_hosts.txt
   fi
-done
 
-if [ $fail_count -gt 0 ]
-then
-  echo "Total number of failed hosts: $fail_count. Please see failed_hosts.txt for a complete list"
-  echo "" >> failed_hosts.txt
-  echo "list generated on: " >> failed_hosts.txt
-  date >> failed_hosts.txt
-  printf '%s\n' "${failed_hosts[@]}" >> failed_hosts.txt
-fi
-
-if [ $fail_count -eq 0 ]
-then
-  echo "$1 is running on all hosts"
+  if [ $fail_count -eq 0 ]
+  then
+    echo "number of hosts checked: ${#host_list[@]}"
+    echo "$1 is running on all hosts"
+  fi
+else
+  echo "ERROR: ./host-list.txt is empty or no such file"
 fi
